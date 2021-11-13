4b937c16a4107430d38e699d6af00c18a9671f23
shaonkislo <shaon6340@agmail.com>
2021-11-13 12:47:26 +0600

    12.47 pm

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -21,7 +21,7 @@ const useFirebase = () => {
             const newUser = {email, displayName: name};
             setUser(newUser)
             // save user to the database
-            saveUser(email, name);    
+            saveUser(email, name, 'POST');    
          // send name to firebase after creation
 
          updateProfile(auth.currentUser, {
@@ -57,7 +57,10 @@ const useFirebase = () => {
       signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
+        saveUser(user.email, user.displayName, 'PUT')
         setAuthError('');
+        const destination = location?.state?.from || '/';
+    history.replace(destination);  
       }).catch((error) => {
         setAuthError (error.message);
       }).finally(() => setIsLoading(false));
@@ -86,10 +89,10 @@ const useFirebase = () => {
           .finally(() => setIsLoading(false));     
     }
 
-    const saveUser = (email, displayName) =>{
+    const saveUser = (email, displayName, method) =>{
         const user = {email, displayName};
         fetch('http://localhost:5000/users', {
-          method:'POST',
+          method: method,
           headers: {
             'content-type':'application/json'
           },
