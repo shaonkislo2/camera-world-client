ae40638f8bce99d4e553346e6f40c52eacab2638
shaonkislo <shaon6340@agmail.com>
2021-11-14 16:47:25 +0600

    final - 3

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -7,7 +7,7 @@ const AboutUs = () => {
 				<h1 className="text-primary">About Us</h1>				
 					<div className="row  d-flex justify-content-center p-5">
 						<div className="col-md-6 mt-5">
-							<h3>The Camera World in DHaka has been under the direction of the Rostom Family since 2000.    We have been in our current location on Bashundhara City Shopping Complex  in Dhaka since the late 1970’s.
+							<h3>The Camera World in DHaka has been under the direction of the Rostom Family since 2000.    We have been in our current location on Bashundhara City Shopping Complex  in Dhaka since the late 1990.
  
 Photography is our passion.  We offer a wide array of equipment including cameras, lenses, tripods, lighting, cases, an d everything in between.  Our services include scanning & archiving, fine art printing, wrapped canvases, custom photo books, cards & invitations, gifting, video transfer, and so much more.  With an in-house graphics and custom framing department, we can create almost anything.
  
